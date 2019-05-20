3518aed5c1b740c86f9c7d6a6440a3a78bed51b0
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-05-20 20:48:38 +0900

    不要な記述を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
-set :rbenv_custom_path, '/root/.rbenv'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/daiki0730.pem']
