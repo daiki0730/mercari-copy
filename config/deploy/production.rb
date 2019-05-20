2c9b43b097b9c304007c87ddfff9ec899ebc71ee
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-05-20 18:56:57 +0900

    デプロイするために記述を変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '3.16.201.36', user: 'ec2-user', roles: %w{app db web}
+server '52.69.166.145', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
